9e27617b3c78cddf169258089912b7bc113f4bc2
KeamogetsweMokoka <your.email@example.com>
2025-06-01 23:51:13 +0200

    Narbar structure created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'; 
+import Navbar from './Components/Navbar.jsx'; 
+import Category from './Components/Categories.jsx';
+
+function App() {
+  
+  return (
+  <>
+    <Navbar/> 
+  </>
+
+  );
+}
+
+export default App; 
---
+++ src/Components/Categories.jsx
@@ -0,0 +1,14 @@
+
+function Category(){
+    return(
+        <ul className='category-container'>
+            <li>All Items</li>
+            <li>Electronics</li>
+            <li>Textbooks</li>
+            <li>Food</li>
+            <li>Clothes</li>
+        </ul>
+    ); 
+}
+
+export default Category;
